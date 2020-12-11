1649d00b7b6ec3d248f635bd23192c80ea369580
Gustavo Santos <gustavofsantos@outlook.com>
2020-12-11 15:11:12 -0300

    center decrypted message

--- pages/secret/[cipher_content].js
+++ pages/secret/[cipher_content].js
@@ -39,7 +39,7 @@ export default function SecretPage() {
 
         {!decryptionLoading && !!data && (
           <MainBox>
-            <p className="break-words">{data}</p>
+            <p className="break-words text-center">{data}</p>
           </MainBox>
         )}
 
